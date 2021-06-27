9b201722ae78a56bb2555e4bd1d4f732f0909a3c
APNeto <penaneto@gmail.com>
2021-06-27 18:41:38 -0300

    Added AddItem component, rendering in App.js

--- inv-management/src/App.js
+++ inv-management/src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import SearchBar from './SearchBar';
+import AddItem from './AddItem';
 import { PropTypes } from 'prop-types';
 import { useState } from 'react';
+
 
 function App() {
   const [name, setName] = useState("");
   const [number, setNumber] = useState(0);
   const [type, setType] = useState("");
   const [brand, setBrand] = useState("");
+
+  const [data, setData] = useState({ items: []});
 
   const updateSearch = (name, number, type, brand) => {
     setName(name);
@@ -16,13 +20,41 @@ function App() {
     setBrand(brand);
   }
 
+  const addItemToData = (item) => {
+    let currentItems = data['items']
+    currentItems.push(item)
+    setData({items: currentItems})
+    console.log(data)
+  }
   return (
     <div className="App">
       <SearchBar setFilters={updateSearch} />
       <p>D'Inventory</p>
       <p>{name}</p>
+      <AddItem addItem={addItemToData}/>
       <ButtonState ></ButtonState>
     </div>
   );
+}
+
+
+function ButtonState() {
+  const [title, setTitle] = useState("")
+  const [count, setCount] = useState(0)
+
+  const updateTitle = () => {
+    setTitle("Ther's a title in here")
+  }
+  const updateCount = () => {
+    setCount(count + 1)
+  }
+  return (
+    <div>
+      <p>Text: {title}</p>
+      <p>Counter: {count}</p>
+      <button onClick={updateTitle}>Update Title</button>
+      <button onClick={updateCount}>Update Counter</button>
+    </div>
+  )
 }
 export default App;
